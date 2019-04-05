b8f6bebe49e02decb72ca3818051fdd1e44309a3
Alain <alaint77@gmail.com>
2019-04-05 16:26:50 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -19,9 +19,16 @@ function coloringSquare() {
   let drawing = document.querySelectorAll(".mySquare");
   drawing.forEach((div) => {
       div.addEventListener('mouseover', function(e) {
-          e.target.setAttribute('style', 'background-color: black;');
+          e.target.setAttribute('style', `background-color: ${randomColors()};`);
       });
   });
 }
 
 coloringSquare();
+
+function randomColors() {
+  let red = Math.floor(Math.random() * 256);
+  let green = Math.floor(Math.random() * 256);
+  let blue = Math.floor(Math.random() * 256);
+  return (`rgb( ${red}, ${green}, ${blue});`);
+}
